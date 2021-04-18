77681e1e29e02cca2dab5ba64c7ea139033ad133
Akshay <akshaymg99@gmail.com>
2021-04-18 13:23:02 +0530

    Added Django Welcome Page

---
+++ speech/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# from .models import
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
