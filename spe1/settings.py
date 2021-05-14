fce6647ab564d338d7537a2b9efcf92a42e4c381
Akshay <akshaymg99@gmail.com>
2021-05-15 02:46:35 +0530

    changed installed apps in settings

--- spe1/settings.py
+++ spe1/settings.py
@@ -78,14 +78,14 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'speech.apps.SpeechConfig',
+    'speech.dependencies',
+    'speech.Analyzer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'speech.dependencies',
-    'speech.Analyzer',
 
 ]
 
